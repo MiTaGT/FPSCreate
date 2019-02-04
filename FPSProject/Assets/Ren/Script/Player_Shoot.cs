833ce53d349fdc0dd739592f8c32614ec1f4fa6b
虹鱒 <renwin25@gmail.com>
2019-02-05 00:32:11 +0900

    射撃の同期。HPゲージと減少

---
+++ FPSProject/Assets/Ren/Script/Player_Shoot.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+/// <summary>
+/// プレイヤーのネットワーク上での当たり判定計算
+/// レイキャスト使用
+/// </summary>
+public class Player_Shoot : NetworkBehaviour {
+    //残り体力、同期対象
+    [SyncVar(hook = "OnLifeChanged")]
+    float m_life = 100;
+
+    //ローカルプレイヤー体力表示テキスト
+    Text m_MyLifeText;
+    Image MyLifeFill;
+
+    Text m_EnemyLifeText;
+
+    private void Start() {
+        //頭上体力表示テキストを取ってくる
+        //ローカルプレイヤー
+        if (isLocalPlayer) { 
+            //見た目を無効化
+            //transform.Find("Avater").gameObject.SetActive(false);
+            //ローカルプレイヤーの残り体力表示テキストを取得
+            m_MyLifeText = GameObject.Find("MyLifeText").GetComponent<Text>();
+            MyLifeFill = GameObject.Find("MyLifeFill").GetComponent<Image>();
+        } else {
+            //他人の時
+            m_EnemyLifeText = GameObject.Find("EnemyLifeText").GetComponent<Text>();
+        }
+        //途中参加用の表示を合わせる
+        OnLifeChanged(m_life);
+    }
+    private void Update() {
+        //他人だったら何もしない
+        if (!isLocalPlayer) {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            //画面中央に向かってレイキャスト
+            Ray ray = new Ray(Camera.main.transform.position,Camera.main.transform.forward);
+
+            Debug.DrawRay(ray.origin,ray.direction * 100,Color.red,100,false);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out  hit)) {
+                //当たったやつがPlayerなら
+                if (hit.collider.CompareTag ("Player")) {
+                    //撃ったことを鯖に通知
+                    CmdShotPlayer(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+    /// <summary>
+    ///ライフを減らす処理。これは鯖で作業
+    /// </summary>
+    [Command]
+    void CmdShotPlayer(GameObject target) {
+        Debug.Log("HIT!!");
+        Debug.Log(target);
+        target.GetComponent<Player_Shoot>().m_life -= 1;
+    }
+    /// <summary>
+    ///変化したときの処理
+    ///体力表示テキストを最新のものに更新
+    /// </summary>
+    void OnLifeChanged(float newValue) {
+        m_life = newValue;
+        if (isLocalPlayer) {
+            m_MyLifeText.text = m_life.ToString();
+            MyLifeFill.fillAmount = m_life / 100;
+        } else {
+            m_EnemyLifeText.text = m_life.ToString();
+        }
+    }
+}
