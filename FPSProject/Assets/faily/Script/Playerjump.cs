23066df845a35120728d772b63f303235e5b1a45
rasinban1116 <patyeri1116@gmail.com>
2019-02-04 22:37:25 +0900

    プレイヤーの視点移動　移動　マウス移動　ジャンプを追加

---
+++ FPSProject/Assets/faily/Script/Playerjump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerjump : MonoBehaviour {
+    public GameObject Player;
+    private Rigidbody PlayerRigid;
+    public float Upspeed;
+    // Use this for initialization
+    void Start()
+    {
+        PlayerRigid = Player.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Grond" && Input.GetKey(KeyCode.Space)) //Groundと接触している、かつスペースキーが押されたとき
+        {
+            PlayerRigid.AddForce(transform.up * 130);
+
+
+
+        }
+
+    }
+}
