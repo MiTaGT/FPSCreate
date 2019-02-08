0a259eb355b8bd88ccd4b73f6c9be579b0ef1cc7
satoka-zu-3 <satokazu339@gmail.com>
2019-02-08 09:27:46 +0900

    銃の追加

---
+++ FPSProject/Assets/Kirito/ScoreDirector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDirector : MonoBehaviour {
+
+    // Use this for initialization
+    public int PlayerSize_A;//Aチームのプレイヤー数
+
+    public int PlayerScore_A[];
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
