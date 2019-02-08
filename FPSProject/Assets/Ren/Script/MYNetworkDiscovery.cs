547f62e9f0559fb9ecd61ac4eebb4ee739249931
niziamsu <renwin25@gmail.com>
2019-02-08 11:03:21 +0900

    ローカル通信に対応させるテストコードを追加

---
+++ FPSProject/Assets/Ren/Script/MYNetworkDiscovery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MYNetworkDiscovery : NetworkDiscovery {
+
+    public override void OnReceivedBroadcast(string fromAddress, string data) {
+        //見つけたアドレスを鯖アドレスに入れる
+        NetworkManager.singleton.networkAddress = fromAddress;
+        //既に接続済みなら何もしない。
+        if (NetworkManager.singleton.IsClientConnected() == true) {
+            return;
+        }
+        Debug.LogFormat("OnReceivedBroadcast;fromAddress:{0},data{1}",fromAddress,data);
+        NetworkManager.singleton.StartClient();
+    }
+}
