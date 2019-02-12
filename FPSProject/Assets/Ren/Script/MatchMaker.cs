4a16101f98bf33f39f19eb2b5cc3f36c7d8f3695
niziamsu <renwin25@gmail.com>
2019-02-12 16:12:26 +0900

    ネットワーク接続のUIを自作のものに変更。ネットワークが接続できないバグは原因不明

---
+++ FPSProject/Assets/Ren/Script/MatchMaker.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+/// <summary>
+/// マッチメーカー(ドラえもん風)
+/// </summary>
+public class MatchMaker : MonoBehaviour {
+    //マッチリスト格納用List
+    List<MatchInfoSnapshot> m_Matchs = null;
+
+    /// <summary>
+    /// スタート
+    /// </summary>
+    private void Start() {
+        //マッチメーカーを使う準備
+        NetworkManager.singleton.StartMatchMaker();
+    }
+
+    /// <summary>
+    /// アップデート
+    /// </summary>
+    private void Update() {
+        NetworkManager.singleton.matchMaker.ListMatches(0,10,"",true,0,0,OnListMatches);
+    }
+    /// <summary>
+    /// マッチ製作終了後呼ばれるもの
+    /// </summary>
+    void OnCreateMatch(bool success, string extendedInfo, MatchInfo matchInfo) {
+        if (success) {
+            NetworkManager.singleton.StartHost(matchInfo);
+        } else {
+            Debug.Log("失敗");
+        }
+    }
+    /// <summary>
+    /// マッチリスト取得後呼ばれるもの
+    /// </summary>
+    private void OnListMatches(bool success,string extendedInfo,List<MatchInfoSnapshot> matches) {
+        if (success) {
+            //成功
+            m_Matchs  = matches;
+        } else {
+            //失敗…
+            Debug.Log("失敗");
+        }
+    }
+    /// <summary>
+    /// マッチ入場時に呼ばれるもの
+    /// </summary>
+    private void OnJoinMatch(bool success, string extendedInfo, MatchInfo matchInfo) {
+        if (success) {
+            //成功
+            NetworkManager.singleton.StartClient(matchInfo);
+        } else {
+            //失敗
+            Debug.Log("失敗");
+        }
+    }
+    /// <summary>
+    /// マッチ制作ボタン用
+    /// </summary>
+    public void MatchCreateButton() {
+        InputField input = GameObject.Find("RoomNameInputField").GetComponent<InputField>();
+        NetworkManager.singleton.matchMaker.CreateMatch(input.text, 7 , true, "" , "" , "" , 0 , 0 , OnCreateMatch);
+    }
+
+    /// <summary>
+    /// マッチ接続ボタン用
+    /// </summary>
+    public void MatchJoin() {
+        Debug.Log(m_Matchs[0].name);
+        NetworkManager.singleton.matchMaker.JoinMatch(m_Matchs[0].networkId,"","","",0,0,OnJoinMatch);
+    }
+}
---
+++ FPSProject/Assets/Ren/Script/NetworkManagerHUD.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+/// <summary>
+/// ネットワーク接続の自作用スクリプト
+/// </summary>
+public class NetworkManagerHUD : MonoBehaviour {
+    //主なUI
+    GameObject m_MainUIs;
+    //接続状態を表示するテキスト
+    GameObject m_ConnectingText;
+    //接続状態種別
+    enum ConnctionState {
+        //サーバー
+        Sever,
+        //ホスト
+        Host,
+        //リモートクライアント接続
+        RemoteClientConnected,
+        //リモートクライアント接続試行中
+        RemoteClientConnecting,
+        //接続なし
+        Nothing,
+    }
+
+    /// <summary>
+    /// ネットワーク接続の状態を取得
+    /// </summary>
+    ConnctionState GetConnectionState() {
+        //サーバーが起動してる？
+        if (NetworkServer.active) {
+            //クライアントか
+            if (NetworkManager.singleton.IsClientConnected()) {
+                //ホストとして起動中！
+                return ConnctionState.Host;
+            } else {
+                //サーバーとして起動中！
+                return ConnctionState.Sever;
+            }
+        } else if (NetworkManager.singleton.IsClientConnected()) {
+            //リモートクライアント接続！
+            return ConnctionState.RemoteClientConnected;
+        } else {
+            NetworkClient client = NetworkManager.singleton.client;
+            //Connectionが存在するか？
+            if (client != null && client.connection != null && client.connection.connectionId != -1) {
+                //接続試行中
+                return ConnctionState.RemoteClientConnecting;
+            } else {
+                //接続なし
+                return ConnctionState.Nothing;
+            }
+        }
+    }
+
+    /// <summary>
+    /// スタート
+    /// </summary>
+    private void Start() {
+        m_MainUIs = GameObject.Find("MainUIs");
+        m_ConnectingText = GameObject.Find("ConnectingText");
+    }
+
+    /// <summary>
+    /// アップデート
+    /// </summary>
+    private void Update() {
+        ConnctionState state = GetConnectionState();
+
+        //接続試行中
+        if (state == ConnctionState.RemoteClientConnecting) {
+            m_MainUIs.SetActive(false);
+            m_ConnectingText.SetActive(true);
+            m_ConnectingText.GetComponent<Text>().text = "接続中…";
+
+            //ESCで中止
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                NetworkManager.singleton.StopHost();
+            }
+        } else {
+            m_MainUIs.SetActive(true);
+            m_ConnectingText.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// サーバー起動ボタン用
+    /// </summary>
+    public void OnServerButton() {
+        NetworkManager.singleton.StartServer();
+    }
+
+    /// <summary>
+    /// ホスト起動用
+    /// </summary>
+    public void OnHostButton() {
+        NetworkManager.singleton.StartHost();
+    }
+
+    /// <summary>
+    /// サーバーへ接続用
+    /// </summary>
+    public void OnClientButton() {
+        InputField input = GameObject.Find("ServerAddressInputField").GetComponent<InputField>();
+        NetworkManager.singleton.networkAddress = input.text;
+        NetworkManager.singleton.StartClient();
+    }
+}
